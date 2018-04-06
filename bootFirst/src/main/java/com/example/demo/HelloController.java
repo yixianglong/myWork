0240b9f7a63c2244d14b6bd6575afbfdd7b92e90
yixianglong <yixiang.long@qq.com>
2018-04-06 23:00:31 +0800

    spring boot增加配置的属性

--- bootFirst/src/main/java/com/example/demo/HelloController.java
+++ bootFirst/src/main/java/com/example/demo/HelloController.java
@@ -1,17 +1,27 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-@Value("${first}")
-private String first;
-    @RequestMapping(value = "/",method =RequestMethod.GET)
-    public String index() {
-        return  first +" from Spring Boot!";
-    }
+	@Autowired
+	private Me me;
+	
+   @GetMapping(value="/")
+	//@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(@RequestParam(value="id" ,required = false,defaultValue="1") Integer myid) {
+		return myid+me.getName() + ":" + me.getAge() + ": from Spring Boot!";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String ssy(@RequestParam("id") Integer myid) {
+		return myid+me.getName() + ":" + me.getAge() + ": from Spring Boot!";
+	}
+	
 
 }
---
+++ bootFirst/src/main/java/com/example/demo/Me.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+@Component
+@ConfigurationProperties(prefix = "first")
+public class Me {
+	private String name;
+	private String age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+}
